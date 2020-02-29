fbbeb0fa08eaba6868575ee8416fdafea3d49b2a
aokea <35871123+aokea@users.noreply.github.com>
2020-02-29 18:06:24 +0000

    new

---
+++ CloudSchool/config/test-output/02-29_17-20-16-678/Suite.js
@@ -0,0 +1,202 @@
+var testData = {"pageData": {
+    "phase": "null",
+    "aut": "null",
+    "sPMap": {},
+    "gridUrl": "Grid.html",
+    "testTags": [],
+    "domain": "null",
+    "configProperties": {"xF-ID": "f3f3fbc8-d922-4b4c-8bc1-bd855119563c"},
+    "startTime": {
+        "dateTime": "02/29/2020-17:20:00",
+        "dateOnly": "02/29/2020",
+        "timeOnly": "17:20:00",
+        "raw": 1582996800707
+    },
+    "endTime": {
+        "dateTime": "02/29/2020-17:20:17",
+        "dateOnly": "02/29/2020",
+        "timeOnly": "17:20:17",
+        "raw": 1582996817172
+    },
+    "folderName": "02-29_17-20-16-678",
+    "reportFolder": "C:\\Users\\abbey\\Desktop\\2019-xFramium\\CloudSchool\\config\\test-output\\02-29_17-20-16-678",
+    "executionSummary": [
+        {
+            "exceptionType": "CLOUD",
+            "ignored": "0",
+            "elementUsageMap": {"null.null.TC01_ValidLogin": {
+                "failCount": "1",
+                "keyName": "null.null.TC01_ValidLogin",
+                "siteName": "null",
+                "pageName": "null",
+                "passCount": "0",
+                "elementName": "TC01_ValidLogin"
+            }},
+            "failed": "1",
+            "sessionId": "null",
+            "callMap": {},
+            "elementUsage": [{
+                "failCount": "1",
+                "keyName": "null.null.TC01_ValidLogin",
+                "siteName": "null",
+                "pageName": "null",
+                "passCount": "0",
+                "elementName": "TC01_ValidLogin"
+            }],
+            "tagNames": [""],
+            "cloud": {
+                "hostName": "127.0.0.1:4444",
+                "proxyPort": "null",
+                "password": "",
+                "keepAlive": "null",
+                "deviceAction": "null",
+                "provider": "SELENIUM",
+                "gridInstance": "null",
+                "name": "local",
+                "description": "",
+                "userName": "",
+                "proxyHost": "null",
+                "gesture": "null"
+            },
+            "total": "1",
+            "testStatus": "FAILED",
+            "pageUsageMap": {"null.null": [
+                "0",
+                "1"
+            ]},
+            "name": "TC01_ValidLogin",
+            "startTime": {
+                "dateTime": "02/29/2020-17:20:16",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:20:16",
+                "raw": 1582996816646
+            },
+            "endTime": {
+                "dateTime": "02/29/2020-17:20:16",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:20:16",
+                "raw": 1582996816666
+            },
+            "passed": "0",
+            "folderName": "Windows chrome\\TC01_ValidLogin",
+            "device": {
+                "capabilities": {"_testName": "TC01_ValidLogin"},
+                "os": "Windows",
+                "active": true,
+                "deviceLock": "java.util.concurrent.Semaphore@7282847e[Permits = 1]",
+                "deviceName": "null",
+                "resolution": "null",
+                "capList": [
+                    "org.xframium.device.DeviceCap@5544fed3",
+                    "org.xframium.device.DeviceCap@2f003ce2",
+                    "org.xframium.device.DeviceCap@49c7ec08"
+                ],
+                "driverType": "WEB",
+                "tagNames": [
+                    "ChromeBrowser",
+                    "Desktop"
+                ],
+                "manufacturer": "Google",
+                "cloud": "local",
+                "environment": "Windows chrome",
+                "phoneNumber": "null",
+                "osVersion": "null",
+                "browserVersion": "null",
+                "availableDevices": "1",
+                "browserName": "chrome",
+                "model": "Chrome",
+                "cachedString": "Google Chrome [Chrome]",
+                "key": "Chrome"
+            }
+        },
+        {
+            "exceptionType": "CLOUD",
+            "ignored": "0",
+            "elementUsageMap": {"null.null.TC02_InValidLogin": {
+                "failCount": "1",
+                "keyName": "null.null.TC02_InValidLogin",
+                "siteName": "null",
+                "pageName": "null",
+                "passCount": "0",
+                "elementName": "TC02_InValidLogin"
+            }},
+            "failed": "1",
+            "sessionId": "null",
+            "callMap": {},
+            "elementUsage": [{
+                "failCount": "1",
+                "keyName": "null.null.TC02_InValidLogin",
+                "siteName": "null",
+                "pageName": "null",
+                "passCount": "0",
+                "elementName": "TC02_InValidLogin"
+            }],
+            "tagNames": [""],
+            "cloud": {
+                "hostName": "127.0.0.1:4444",
+                "proxyPort": "null",
+                "password": "",
+                "keepAlive": "null",
+                "deviceAction": "null",
+                "provider": "SELENIUM",
+                "gridInstance": "null",
+                "name": "local",
+                "description": "",
+                "userName": "",
+                "proxyHost": "null",
+                "gesture": "null"
+            },
+            "total": "1",
+            "testStatus": "FAILED",
+            "pageUsageMap": {"null.null": [
+                "0",
+                "1"
+            ]},
+            "name": "TC02_InValidLogin",
+            "startTime": {
+                "dateTime": "02/29/2020-17:20:16",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:20:16",
+                "raw": 1582996816853
+            },
+            "endTime": {
+                "dateTime": "02/29/2020-17:20:16",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:20:16",
+                "raw": 1582996816853
+            },
+            "passed": "0",
+            "folderName": "Windows chrome\\TC02_InValidLogin",
+            "device": {
+                "capabilities": {"_testName": "TC01_ValidLogin"},
+                "os": "Windows",
+                "active": true,
+                "deviceLock": "java.util.concurrent.Semaphore@7282847e[Permits = 1]",
+                "deviceName": "null",
+                "resolution": "null",
+                "capList": [
+                    "org.xframium.device.DeviceCap@5544fed3",
+                    "org.xframium.device.DeviceCap@2f003ce2",
+                    "org.xframium.device.DeviceCap@49c7ec08"
+                ],
+                "driverType": "WEB",
+                "tagNames": [
+                    "ChromeBrowser",
+                    "Desktop"
+                ],
+                "manufacturer": "Google",
+                "cloud": "local",
+                "environment": "Windows chrome",
+                "phoneNumber": "null",
+                "osVersion": "null",
+                "browserVersion": "null",
+                "availableDevices": "1",
+                "browserName": "chrome",
+                "model": "Chrome",
+                "cachedString": "Google Chrome [Chrome]",
+                "key": "Chrome"
+            }
+        }
+    ],
+    "suiteName": "CloudSchool"
+}};
---
+++ CloudSchool/config/test-output/02-29_17-22-33-266/Suite.js
@@ -0,0 +1,454 @@
+var testData = {"pageData": {
+    "phase": "null",
+    "aut": "CloudSchool (WEB)",
+    "sPMap": {},
+    "gridUrl": "Grid.html",
+    "testTags": [],
+    "domain": "null",
+    "configProperties": {"xF-ID": "d0a3690f-ae63-4124-8013-dfcc4dee6111"},
+    "startTime": {
+        "dateTime": "02/29/2020-17:22:10",
+        "dateOnly": "02/29/2020",
+        "timeOnly": "17:22:10",
+        "raw": 1582996930715
+    },
+    "endTime": {
+        "dateTime": "02/29/2020-17:22:51",
+        "dateOnly": "02/29/2020",
+        "timeOnly": "17:22:51",
+        "raw": 1582996971458
+    },
+    "folderName": "02-29_17-22-33-266",
+    "reportFolder": "C:\\Users\\abbey\\Desktop\\2019-xFramium\\CloudSchool\\config\\test-output\\02-29_17-22-33-266",
+    "executionSummary": [
+        {
+            "exceptionType": "null",
+            "ignored": "0",
+            "elementUsageMap": {
+                "null.CSHomePage.MrAdmin_topnav.CLICKABLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.MrAdmin_topnav.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "MrAdmin_topnav.CLICKABLE"
+                },
+                "null.CSLoginPage.Signin_btn": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Signin_btn",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Signin_btn"
+                },
+                "null.CSHomePage.Logout_link.CLICKABLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.Logout_link.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "Logout_link.CLICKABLE"
+                },
+                "null.CSLoginPage.Username_txtbox": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Username_txtbox"
+                },
+                "null.CSHomePage.MrAdmin_topnav": {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.MrAdmin_topnav",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "MrAdmin_topnav"
+                },
+                "null.CSLoginPage.CloudScl_img.VISIBLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.CloudScl_img.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "CloudScl_img.VISIBLE"
+                },
+                "null.CSHomePage.Logout_link": {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.Logout_link",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "Logout_link"
+                },
+                "null.CSLoginPage.Password_txtbox": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Password_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Password_txtbox"
+                },
+                "null.CSLoginPage.Username_txtbox.CLICKABLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Username_txtbox.CLICKABLE"
+                }
+            },
+            "failed": "0",
+            "sessionId": "2bd79b6b-d215-4d28-80bb-2045d80b5a74",
+            "callMap": {
+                "Login_Function": [
+                    "1",
+                    "0"
+                ],
+                "Logout_Function": [
+                    "1",
+                    "0"
+                ]
+            },
+            "elementUsage": [
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.Logout_link",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "Logout_link"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.Logout_link.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "Logout_link.CLICKABLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.MrAdmin_topnav",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "MrAdmin_topnav"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSHomePage.MrAdmin_topnav.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSHomePage",
+                    "passCount": "1",
+                    "elementName": "MrAdmin_topnav.CLICKABLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.CloudScl_img.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "CloudScl_img.VISIBLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Password_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Password_txtbox"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Signin_btn",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Signin_btn"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Username_txtbox"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Username_txtbox.CLICKABLE"
+                }
+            ],
+            "tagNames": [""],
+            "cloud": {
+                "hostName": "127.0.0.1:4444",
+                "proxyPort": "null",
+                "password": "",
+                "keepAlive": "null",
+                "deviceAction": "null",
+                "provider": "SELENIUM",
+                "gridInstance": "null",
+                "name": "local",
+                "description": "",
+                "userName": "",
+                "proxyHost": "null",
+                "gesture": "null"
+            },
+            "total": "14",
+            "testStatus": "PASSED",
+            "pageUsageMap": {
+                "null.CSLoginPage": [
+                    "5",
+                    "0"
+                ],
+                "null.CSHomePage": [
+                    "4",
+                    "0"
+                ]
+            },
+            "name": "TC01_ValidLogin",
+            "startTime": {
+                "dateTime": "02/29/2020-17:22:28",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:22:28",
+                "raw": 1582996948952
+            },
+            "endTime": {
+                "dateTime": "02/29/2020-17:22:33",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:22:33",
+                "raw": 1582996953264
+            },
+            "passed": "14",
+            "folderName": "Windows chrome\\TC01_ValidLogin",
+            "device": {
+                "capabilities": {},
+                "os": "WINDOWS",
+                "active": false,
+                "deviceLock": "java.util.concurrent.Semaphore@329547d2[Permits = 1]",
+                "deviceName": "Chrome",
+                "resolution": "1050 x 708",
+                "capList": [],
+                "driverType": "WEB",
+                "tagNames": [],
+                "manufacturer": "Microsoft Corporation",
+                "cloud": "local",
+                "environment": "WINDOWS 10 Chrome 8",
+                "phoneNumber": "null",
+                "osVersion": "10",
+                "browserVersion": "null",
+                "availableDevices": "1",
+                "browserName": "Chrome 8",
+                "model": "null",
+                "cachedString": "null",
+                "key": "Chrome"
+            }
+        },
+        {
+            "exceptionType": "null",
+            "ignored": "0",
+            "elementUsageMap": {
+                "null.CSLoginPage.Signin_btn": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Signin_btn",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Signin_btn"
+                },
+                "null.CSLoginPage.Username_error_text.VISIBLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_error_text.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Username_error_text.VISIBLE"
+                },
+                "null.CSLoginPage.Username_txtbox": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Username_txtbox"
+                },
+                "null.CSLoginPage.Password_error_text.VISIBLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Password_error_text.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Password_error_text.VISIBLE"
+                },
+                "null.CSLoginPage.CloudScl_img.VISIBLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.CloudScl_img.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "CloudScl_img.VISIBLE"
+                },
+                "null.CSLoginPage.Password_txtbox": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Password_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Password_txtbox"
+                },
+                "null.CSLoginPage.Username_txtbox.CLICKABLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Username_txtbox.CLICKABLE"
+                },
+                "null.CSLoginPage.UsernamePassword_error_panel.VISIBLE": {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.UsernamePassword_error_panel.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "UsernamePassword_error_panel.VISIBLE"
+                }
+            },
+            "failed": "0",
+            "sessionId": "8d741fb9-cba1-4ca5-a1ab-963976fd702c",
+            "callMap": {"InvalidLogin_Function": [
+                "1",
+                "0"
+            ]},
+            "elementUsage": [
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.CloudScl_img.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "CloudScl_img.VISIBLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Password_error_text.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Password_error_text.VISIBLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Password_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Password_txtbox"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Signin_btn",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Signin_btn"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.UsernamePassword_error_panel.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "UsernamePassword_error_panel.VISIBLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_error_text.VISIBLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Username_error_text.VISIBLE"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "2",
+                    "elementName": "Username_txtbox"
+                },
+                {
+                    "failCount": "0",
+                    "keyName": "null.CSLoginPage.Username_txtbox.CLICKABLE",
+                    "siteName": "null",
+                    "pageName": "CSLoginPage",
+                    "passCount": "1",
+                    "elementName": "Username_txtbox.CLICKABLE"
+                }
+            ],
+            "tagNames": [""],
+            "cloud": {
+                "hostName": "127.0.0.1:4444",
+                "proxyPort": "null",
+                "password": "",
+                "keepAlive": "null",
+                "deviceAction": "null",
+                "provider": "SELENIUM",
+                "gridInstance": "null",
+                "name": "local",
+                "description": "",
+                "userName": "",
+                "proxyHost": "null",
+                "gesture": "null"
+            },
+            "total": "16",
+            "testStatus": "PASSED",
+            "pageUsageMap": {"null.CSLoginPage": [
+                "13",
+                "0"
+            ]},
+            "name": "TC02_InValidLogin",
+            "startTime": {
+                "dateTime": "02/29/2020-17:22:46",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:22:46",
+                "raw": 1582996966981
+            },
+            "endTime": {
+                "dateTime": "02/29/2020-17:22:49",
+                "dateOnly": "02/29/2020",
+                "timeOnly": "17:22:49",
+                "raw": 1582996969749
+            },
+            "passed": "16",
+            "folderName": "Windows chrome\\TC02_InValidLogin",
+            "device": {
+                "capabilities": {},
+                "os": "WINDOWS",
+                "active": false,
+                "deviceLock": "java.util.concurrent.Semaphore@7fa35d57[Permits = 1]",
+                "deviceName": "Chrome",
+                "resolution": "1050 x 708",
+                "capList": [],
+                "driverType": "WEB",
+                "tagNames": [],
+                "manufacturer": "Microsoft Corporation",
+                "cloud": "local",
+                "environment": "WINDOWS 10 Chrome 8",
+                "phoneNumber": "null",
+                "osVersion": "10",
+                "browserVersion": "null",
+                "availableDevices": "1",
+                "browserName": "Chrome 8",
+                "model": "null",
+                "cachedString": "null",
+                "key": "Chrome"
+            }
+        }
+    ],
+    "suiteName": "CloudSchool"
+}};
